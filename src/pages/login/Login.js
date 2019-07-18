702b44dc23359ed5fc05bae2691ec0ce283df112
Nazeli <n.terpetrosyan.y@tumo.org>
2019-07-18 19:01:06 +0400

    Created logi UI

--- src/pages/login/Login.js
+++ src/pages/login/Login.js
@@ -1,8 +1,10 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 
-// TODO: use --> import {Redirect} from 'react-router-dom';
-import {Container} from 'react-bootstrap';
+import './login.css';
+
+import {Redirect} from 'react-router-dom';
+import {Container, Form, Button} from 'react-bootstrap';
 
 /**
  * Component for Login Page
@@ -13,17 +15,30 @@ export default class Login extends Component {
   // }
   render() {
     // TODO: use to redirect if user not logged in
-    // if (this.props.user) {
-    //   return (
-    //     <Redirect to={{
-    //       pathname: '/profile',
-    //     }} />
-    //   )
-    // }
+    if (this.props.user) {
+      return (
+        <Redirect to={{
+          pathname: '/profile',
+        }} />
+      )
+    }
     return (
-      <Container className="mt-3">
-        <div>TODO: create login form</div>
-      </Container>
+        <div className="contain">
+        <div className="login">
+        <h1 className="lgn_label">Login</h1>
+        <Form>
+          <Form.Group controlId="formBasicEmail">
+            <Form.Control className="input" type="email" placeholder="Username" />
+          </Form.Group>
+          <Form.Group controlId="formBasicPassword">
+            <Form.Control className="input" type="password" placeholder="Password" />
+          </Form.Group>
+          <Button className="but" variant="primary" type="submit">
+            Submit
+          </Button>
+        </Form>
+        </div>
+        </div>
     )
   }
 }
