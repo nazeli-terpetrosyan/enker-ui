394831f84f810cf33dc5fecec6cdf210fe79b82e
Nazeli <n.terpetrosyan.y@tumo.org>
2019-07-19 17:46:04 +0400

    Started on Signup page

--- src/pages/login/Login.js
+++ src/pages/login/Login.js
@@ -4,7 +4,8 @@ import PropTypes from 'prop-types';
 import './login.css';
 
 import {Redirect} from 'react-router-dom';
-import {Container, Form, Button} from 'react-bootstrap';
+import {Form, Button} from 'react-bootstrap';
+import { Link } from "react-router-dom";
 
 /**
  * Component for Login Page
@@ -28,11 +29,13 @@ export default class Login extends Component {
         <h1 className="lgn_label">Login</h1>
         <Form>
           <Form.Group controlId="formBasicEmail">
-            <Form.Control className="input" type="email" placeholder="Username" />
+            <Form.Control className="input" type="email" placeholder="Email Address" />
           </Form.Group>
           <Form.Group controlId="formBasicPassword">
             <Form.Control className="input" type="password" placeholder="Password" />
           </Form.Group>
+          <span className="signup">Don't have an account? Sign up <Link to="/signup" className="sign_l">here</Link>!</span>
+          <br />
           <Button className="but" variant="primary" type="submit">
             Submit
           </Button>
